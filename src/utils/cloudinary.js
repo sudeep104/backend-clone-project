f1267e2a70099810041eb10b056092c551458b15
Sudeep Nagda <sudeep123nagda@gmail.com>
2025-01-09 13:11:42 +0530

    file upload multer middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,38 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+
+const uploadCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // upload file on cloudinary server
+        const response = await cloudinary.uploader.upload(localFilePath, {resource_type: "auto"
+
+        })
+        // file has been uploaded successfully
+        console.log("File uploaded successfully ", response.url);
+        return response
+        
+    } catch (error) {
+        fs.unlink(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+    }
+}
+
+
+export { uploadCloudinary }
+
+
+// cloudinary.v2.uploader.upload(
+//   "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+//   { public_id: "olympic_flag", tags: "sample_tag" },
+//   function (error, result) {
+//     console.log(result);
+//   }
+// );
